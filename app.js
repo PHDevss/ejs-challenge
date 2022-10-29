d24baefcaeb7550fe5b33ec62606d362903ecb40
Paulo Henrique <97169087+PHDevss@users.noreply.github.com>
2022-10-29 12:08:57 -0300

    Remove spaces

--- app.js
+++ app.js
@@ -56,17 +56,6 @@ app.post('/compose', function (req , res) {
     res.redirect('/posts')
 })
 
-
-
-
-
-
-
-
-
-
-
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
